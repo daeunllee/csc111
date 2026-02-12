1219bc9cd6d44061916e7d14af90e49fc9767910
daeunllee <daeun.lee.dana@gmail.com>
2026-02-12 02:03:23 -0500

    Add files via upload

---
+++ game_entities.py
@@ -0,0 +1,103 @@
+"""CSC111 Project 1: Text Adventure Game - Game Entities
+
+Instructions (READ THIS FIRST!)
+===============================
+
+This Python module contains the entity classes for Project 1, to be imported and used by
+ the `adventure` module.
+ Please consult the project handout for instructions and details.
+
+Copyright and Usage Information
+===============================
+
+This file is provided solely for the personal and private use of students
+taking CSC111 at the University of Toronto St. George campus. All forms of
+distribution of this code, whether as given or with any changes, are
+expressly prohibited. For more information on copyright for CSC111 materials,
+please consult our Course Syllabus.
+
+This file is Copyright (c) 2026 CSC111 Teaching Team
+"""
+from dataclasses import dataclass
+
+
+@dataclass
+class Location:
+    """A location in our text adventure game world.
+
+    Instance Attributes:
+        - id_num: A unique integer representing a certain location.
+        - brief_description: A short and simple description of a certain location.
+        - long_description: A longer, more in-depth, detailed description of a certain location.
+        - available_commands: A dictionary mapping directional commands (in strings) to an id number of the destination
+        location.
+        - items: A list storing all the available items at a certain location.
+        - visited: Whether the player has visited this location before.
+
+    Representation Invariants:
+        - self.id_num >= 0
+        - self.brief_description != ""
+        - self.long_description != ""
+        - all(command != "" for command in self.available_commands)
+    """
+
+    # This is just a suggested starter class for Location.
+    # You may change/add parameters and the data available for each Location object as you see fit.
+    #
+    # The only thing you must NOT change is the name of this class: Location.
+    # All locations in your game MUST be represented as an instance of this class.
+
+    id_num: int
+    brief_description: str
+    long_description: str
+    available_commands: dict[str, int]
+    items: list[str]
+    visited: bool = False
+
+
+@dataclass
+class Item:
+    """An item in our text adventure game world.
+
+    Instance Attributes:
+        - name: Name of the item.
+        - start_position: Which location this item can be initially found in.
+        - target_position: Which location this item can be used at.
+        - target_points: The amount of points earned from finding this item.
+
+    Representation Invariants:
+        - self.name != ""
+        - self.start_position >= 0
+        - self.target_position >= 0
+        - self.target_points >= 0
+    """
+
+    # NOTES:
+    # This is just a suggested starter class for Item.
+    # You may change these parameters and the data available for each Item object as you see fit.
+    # (The current parameters correspond to the example in the handout).
+    #
+    # The only thing you must NOT change is the name of this class: Item.
+    # All item objects in your game MUST be represented as an instance of this class.
+
+    name: str
+    start_position: int
+    target_position: int
+    target_points: int
+
+
+# Note: Other entities you may want to add, depending on your game plan:
+# - Puzzle class to represent special locations (could inherit from Location class if it seems suitable)
+# - Player class
+# etc.
+
+if __name__ == "__main__":
+    pass
+    # When you are ready to check your work with python_ta, uncomment the following lines.
+    # (Delete the "#" and space before each line.)
+    # IMPORTANT: keep this code indented inside the "if __name__ == '__main__'" block
+    # import python_ta
+    # python_ta.check_all(config={
+    #     'max-line-length': 120,
+    #     'disable': ['R1705', 'E9998', 'E9999', 'static_type_checker']
+    # })

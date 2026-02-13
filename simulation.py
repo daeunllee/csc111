cfd38847ef42fbc971ec1aca5ed2f5a4984ee364
daeunllee <daeun.lee.dana@gmail.com>
2026-02-13 12:38:36 -0500

    Update simulation.py

--- simulation.py
+++ simulation.py
@@ -134,7 +134,7 @@ if __name__ == "__main__":
                  "read", "look", "inventory", "go north", "go south", "read", "look", "inventory", "go north",
                  "go south", "read", "look", "inventory", "go north", "go south", "read", "look", "inventory",
                  "go north", "go south", "read", "look", "inventory", "go north", "go south", "read", "look",
-                 "inventory", "go north"]
+                 "inventory"]
     expected_log = [1, 2, 1, 1, 1, 1, 2, 1, 1, 1, 1, 2, 1, 1, 1, 1, 2, 1, 1, 1, 1, 2, 1, 1, 1, 1, 2, 1, 1, 1, 1, 2, 1,
                     1, 1, 1, 2, 1, 1, 1, 1, 2, 1, 1, 1, 1, 2, 1, 1, 1, 1, 2, 1, 1, 1, 1, 2, 1, 1, 1, 1, 2, 1, 1, 1, 1,
                     2, 1, 1, 1, 1, 2, 1, 1, 1, 1, 2, 1, 1, 1, 1, 2, 1, 1, 1, 1, 2, 1, 1, 1, 1, 2, 1, 1, 1, 1, 2, 1, 1,

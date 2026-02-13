d8acc385bb78f021bcb82e42176dc74a6ed67a43
daeunllee <daeun.lee.dana@gmail.com>
2026-02-13 10:18:40 -0500

    Update game_entities.py

--- game_entities.py
+++ game_entities.py
@@ -29,7 +29,7 @@ class Location:
         - id_num: A unique integer representing a certain location.
         - brief_description: A short and simple description of a certain location.
         - long_description: A longer, more in-depth, detailed description of a certain location.
-        - read_description: The description that is printed when the 'read' command is selected.
+        - read_description: Additional hints or other information in a certain location.
         - item_description: Displays a brief description of the item that can be picked up at the location.
         - available_commands: A dictionary mapping directional commands (in strings) to an id number of the destination
         location.
